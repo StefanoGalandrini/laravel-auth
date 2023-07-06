2d75ed4a79b03f333decaeb474652e3c850bd2c3
StefanoGalandrini <stefe68@gmail.com>
2023-07-06 18:18:35 +0200

    Fix navbar and some style for login and register

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-fluid">
-		<a class="navbar-brand" href="{{ route('guests.home') }}">Navbar</a>
+		<span class="navbar-brand h1">PROGETTI</span>
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -9,16 +9,18 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 				<li class="nav-item">
-					<a class="nav-link" href{{ route('admin.dashboard') }}">Dashboard</a>
+					<a class="nav-link" href="{{ route('guests.home') }}">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
 				</li>
 				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="{{ route('admin.projects.create') }}" role="button"
-						data-bs-toggle="dropdown" aria-expanded="false">
+					<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 						NOME RISORSE
 					</a>
 					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="#">Index</a></li>
-						<li><a class="dropdown-item" href="#">Another action</a></li>
+						<li><a class="dropdown-item" href="{{ route('admin.projects.index') }}">Index</a></li>
+						<li><a class="dropdown-item" href="{{ route('admin.projects.create') }}">Create</a></li>
 					</ul>
 				</li>
 				{{-- <li class="nav-item">
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,32 @@
 @extends('guests.layouts.base')
 
 @section('contents')
-	<form method="POST" action="{{ route('login') }}">
-		@csrf
+	<div class="wrapper" style="width: 30vw;">
+		<form method="POST" action="{{ route('login') }}" class="p-5">
+			@csrf
 
-		{{-- Email Address --}}
-		<div class="mb-3">
-			<label for="email" class="form-label">Email</label>
-			<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required autofocus
-				autocomplete="username" value="{{ old('email') }}">
-		</div>
+			{{-- Email Address --}}
+			<div class="mb-3">
+				<label for="email" class="form-label">Email</label>
+				<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required
+					autofocus autocomplete="username" value="{{ old('email') }}">
+			</div>
 
-		{{-- Password --}}
-		<div class="mb-3">
-			<label for="password" class="form-label">Password</label>
-			<input type="password" class="form-control" id="password" name="password" required autocomplete="current-password">
-		</div>
+			{{-- Password --}}
+			<div class="mb-3">
+				<label for="password" class="form-label">Password</label>
+				<input type="password" class="form-control" id="password" name="password" required autocomplete="current-password">
+			</div>
 
-		{{-- Checkbox Remember Me --}}
-		<div class="mb-3 form-check">
-			<input type="checkbox" class="form-check-input" id="remember" name="remember">
-			<label class="form-check-label" for="remember">Remember me</label>
-		</div>
+			{{-- Checkbox Remember Me --}}
+			<div class="mb-3 form-check">
+				<input type="checkbox" class="form-check-input" id="remember" name="remember">
+				<label class="form-check-label" for="remember">Remember me</label>
+			</div>
 
-		<a href="{{ route('password.request') }}">Forgot your password?</a>
+			<a href="{{ route('password.request') }}">Forgot your password?</a>
 
-		<button type="submit" class="btn btn-secondary ms-3">Login</button>
-	</form>
+			<button type="submit" class="btn btn-secondary ms-3">Login</button>
+		</form>
+	</div>
 @endsection
