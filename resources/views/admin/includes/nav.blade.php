24ee89448bc3ab9d0cac993cae1b4aeffd7794c4
StefanoGalandrini <stefe68@gmail.com>
2023-07-06 20:39:56 +0200

    Fix Navbar

--- resources/views/admin/includes/nav.blade.php
+++ resources/views/admin/includes/nav.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 	<div class="container-fluid">
-		<span class="navbar-brand h1">PROGETTI</span>
+		<a class="navbar-brand h1" href="{{ route('guests.home') }}">MY PROJECTS</a>
+
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
 			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
@@ -8,15 +9,15 @@
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-				<li class="nav-item">
+				{{-- <li class="nav-item">
 					<a class="nav-link" href="{{ route('guests.home') }}">Home</a>
-				</li>
+				</li> --}}
 				<li class="nav-item">
 					<a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						NOME RISORSE
+						PROJECTS
 					</a>
 					<ul class="dropdown-menu">
 						<li><a class="dropdown-item" href="{{ route('admin.projects.index') }}">Index</a></li>
@@ -25,7 +26,7 @@
 				</li>
 			</ul>
 
-			<a href="{{ route('admin.projects.index') }}" class="btn btn-success">Back to Index</a>
+			<a href="{{ route('admin.projects.index') }}" class="btn btn-success">Project Index</a>
 
 		</div>
 	</div>
