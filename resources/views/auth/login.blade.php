a86298118506120f31a3d5355294e4aa62b454d6
StefanoGalandrini <stefe68@gmail.com>
2023-07-06 14:38:29 +0200

    Modify login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,30 @@
 @extends('guests.layouts.base')
 
 @section('contents')
-	<form method="post" action="{{ route('login') }}">
+	<form method="POST" action="{{ route('login') }}">
 		@csrf
 
+		{{-- Email Address --}}
 		<div class="mb-3">
 			<label for="email" class="form-label">Email</label>
 			<input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email" required autofocus
 				autocomplete="username" value="{{ old('email') }}">
 		</div>
 
+		{{-- Password --}}
 		<div class="mb-3">
 			<label for="password" class="form-label">Password</label>
 			<input type="password" class="form-control" id="password" name="password" required autocomplete="current-password">
 		</div>
 
+		{{-- Checkbox Remember Me --}}
 		<div class="mb-3 form-check">
 			<input type="checkbox" class="form-check-input" id="remember" name="remember">
 			<label class="form-check-label" for="remember">Remember me</label>
 		</div>
 
-		<a href="{{ route('password.request') }}">
-			Forgot your password?
-		</a>
+		<a href="{{ route('password.request') }}">Forgot your password?</a>
 
-		<button type="submit" class="btn btn-primary">Login</button>
+		<button type="submit" class="btn btn-primary ms-3">Login</button>
 	</form>
 @endsection
--- resources/views/guests/layouts/base.blade.php
+++ resources/views/guests/layouts/base.blade.php
@@ -5,17 +5,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>Guest Base Login</title>
 	@vite('resources/js/app.js')
 </head>
 
 <body>
 	@include('guests.includes.header')
-	<div class="container">
+
+	<div class="container d-flex align-items-center justify-content-center mt-5 mb-5 p-5 rounded border"
+		style="width: 30vw;">
 		<main>
 			@yield('contents')
 		</main>
 	</div>
+
 
 	@include('guests.includes.footer')
 </body>
